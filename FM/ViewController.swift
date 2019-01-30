b965b3fefa7ce61190ed0989b8f3c4d4254d01ad
Caitlyn Chen <caitlynachen@gmail.com>
2019-01-30 11:58:41 -0800

    fm

---
+++ FM/ViewController.swift
@@ -0,0 +1,231 @@
+//
+//  ViewController.swift
+//  Facemine
+//
+//  Created by Caitlyn Chen on 1/30/19.
+//  Copyright © 2019 Caitlyn Chen. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import CommonCrypto
+import CryptoSwift
+
+class ViewController: UIViewController{
+    
+    
+    
+    @IBOutlet weak var startButton: UIButton!
+    var captureSession = AVCaptureSession()
+    
+    var backCamera: AVCaptureDevice?
+    var frontCamera: AVCaptureDevice?
+    var currentDevice: AVCaptureDevice?
+    
+    var photoOutput: AVCapturePhotoOutput?
+    
+    var cameraPreviewLayer:AVCaptureVideoPreviewLayer?
+    
+    var image: UIImage?
+    
+    var toggleCameraGestureRecognizer = UISwipeGestureRecognizer()
+    
+    var zoomInGestureRecognizer = UISwipeGestureRecognizer()
+    var zoomOutGestureRecognizer = UISwipeGestureRecognizer()
+    
+    var timer: Timer!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        
+    }
+    
+    @IBAction func startButtonTapped(_ sender: Any) {
+        setupCaptureSession()
+        setupDevice()
+        setupInputOutput()
+        setupPreviewLayer()
+        captureSession.startRunning()
+        
+        toggleCameraGestureRecognizer.direction = .up
+        toggleCameraGestureRecognizer.addTarget(self, action: #selector(self.switchCamera))
+        
+        view.addGestureRecognizer(toggleCameraGestureRecognizer)
+        
+        // Zoom In recognizer
+        zoomInGestureRecognizer.direction = .right
+        zoomInGestureRecognizer.addTarget(self, action: #selector(zoomIn))
+        view.addGestureRecognizer(zoomInGestureRecognizer)
+        
+        // Zoom Out recognizer
+        zoomOutGestureRecognizer.direction = .left
+        zoomOutGestureRecognizer.addTarget(self, action: #selector(zoomOut))
+        view.addGestureRecognizer(zoomOutGestureRecognizer)
+        
+        startButton.isHidden = true
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: true)
+        
+        
+        
+        
+        
+        
+    }
+    
+    var count = 0
+    @objc func runTimedCode(){
+        if count < 10 {
+            let settings = AVCapturePhotoSettings()
+            self.photoOutput?.capturePhoto(with: settings, delegate: self)
+            
+            count = count + 1
+        } else{
+            timer.invalidate()
+            
+            print("done")
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
+    func setupCaptureSession() {
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+    }
+    
+    func setupDevice() {
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
+        let devices = deviceDiscoverySession.devices
+        
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.back {
+                backCamera = device
+            } else if device.position == AVCaptureDevice.Position.front {
+                frontCamera = device
+            }
+        }
+        currentDevice = frontCamera
+    }
+    
+    func setupInputOutput() {
+        do {
+            
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentDevice!)
+            captureSession.addInput(captureDeviceInput)
+            photoOutput = AVCapturePhotoOutput()
+            photoOutput!.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey : AVVideoCodecType.jpeg])], completionHandler: nil)
+            captureSession.addOutput(photoOutput!)
+            
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    func setupPreviewLayer() {
+        self.cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        self.cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        self.cameraPreviewLayer?.frame = view.frame
+        
+        self.view.layer.insertSublayer(self.cameraPreviewLayer!, at: 0)
+    }
+    
+    @objc func switchCamera() {
+        captureSession.beginConfiguration()
+        
+        
+        // Remove all inputs from the session
+        for input in captureSession.inputs {
+            captureSession.removeInput(input as! AVCaptureDeviceInput)
+        }
+        
+        // Change to the new input
+        let cameraInput:AVCaptureDeviceInput
+        do {
+            cameraInput = try AVCaptureDeviceInput(device: currentDevice!)
+        } catch {
+            print(error)
+            return
+        }
+        
+        if captureSession.canAddInput(cameraInput) {
+            captureSession.addInput(cameraInput)
+        }
+        
+        
+        captureSession.commitConfiguration()
+    }
+    
+    @objc func zoomIn() {
+        if let zoomFactor = currentDevice?.videoZoomFactor {
+            if zoomFactor < 5.0 {
+                let newZoomFactor = min(zoomFactor + 1.0, 5.0)
+                do {
+                    try currentDevice?.lockForConfiguration()
+                    currentDevice?.ramp(toVideoZoomFactor: newZoomFactor, withRate: 1.0)
+                    currentDevice?.unlockForConfiguration()
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    @objc func zoomOut() {
+        if let zoomFactor = currentDevice?.videoZoomFactor {
+            if zoomFactor > 1.0 {
+                let newZoomFactor = max(zoomFactor - 1.0, 1.0)
+                do {
+                    try currentDevice?.lockForConfiguration()
+                    currentDevice?.ramp(toVideoZoomFactor: newZoomFactor, withRate: 1.0)
+                    currentDevice?.unlockForConfiguration()
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    
+    
+}
+
+extension ViewController: AVCapturePhotoCaptureDelegate {
+    
+
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let imageData = photo.fileDataRepresentation() {
+            image = UIImage(data: imageData)
+            
+            let key = "ccC2H19lDDbQDfakxcrtNMQdd0FloLGG" // length == 32
+            let iv = "ggGGHUiDD0Qjhuvv"
+            
+            guard let encrypted = try? ChaCha20(key: key, iv: iv).encrypt(imageData.bytes) else{
+                print("error")
+                return
+            }
+            
+            let pointer = UnsafeBufferPointer(start:encrypted, count:8)
+            let data = Data(buffer:pointer)
+//            try! data.write(to: URL(fileURLWithPath: "myFile.data"))
+
+           
+        }
+    }
+    
+    
+}
+
+
